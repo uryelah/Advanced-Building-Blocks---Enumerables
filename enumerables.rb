8332dfe38e9639755b73409cfee1ddb9da35aa89
sarah <sarachamorro@gmail.com>
2019-10-22 22:34:49 -0300

    STICKLER CORRECTIONS \n delete for ignore comment because now rubocop says its unnecesary

--- enumerables.rb
+++ enumerables.rb
@@ -2,13 +2,13 @@
 
 module Enumerable
   def my_each
-    for i in 0...self.length do # rubocop:disable Style/For
+    for i in 0...self.length do
       yield(self[i])
     end
   end
 
   def my_each_with_index
-    for i in 0...self.length do # rubocop:disable Style/For
+    for i in 0...self.length do
       yield(self[i], i)
     end
   end
