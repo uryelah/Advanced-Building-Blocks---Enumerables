5f02ed6ee382994032c03161f302c61076b8f419
majovainilla <maria_reyes@me.com>
2019-10-30 14:58:17 -0600

    ADD TESTING TO MY_ALL METHOD
    Check for truthy and falsy values of:
    enum with block given
    enum with pattern given
    enum with argument given
    enum with no argument

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -4,10 +4,12 @@ require './enumerables.rb'
 
 RSpec.describe Enumerable do
   let(:num_arr) { [2, 10, 1, 100, 12, -10] }
+  let(:falsy_arr) { [nil, 10, 1, 100, 12, -10] }
+  let(:equal_arr) { [100, 100, 100] }
   let(:str_arr) { ['2', '10', '1', '100', '12', '-10'] }
   let(:range) { (1..5) }
   let(:hash) { { a: 1, b: 2, c: 3 } }
-  
+
   describe 'my_each' do
     it 'Returns an enumerator unless a block is given' do
       expect(num_arr.my_each).to be_kind_of(Enumerator)
@@ -29,6 +31,16 @@ RSpec.describe Enumerable do
     it 'Should return itself if given a block' do
       expect(num_arr.my_each { |n| n }).to eql(num_arr)
     end
+
+    it 'Should work with ranges' do
+      expect(range.my_each { |_e| }).to eql(range)
+    end
+
+    it 'Should accept hash\'s keys and values as parameters if block given' do
+      empty_str = ''
+      hash.my_each { |key, val| empty_str += key.to_s + val.to_s }
+      expect(empty_str).to eql('a1b2c3')
+    end
   end
 
   describe 'my_each_with_index' do
@@ -41,6 +53,10 @@ RSpec.describe Enumerable do
       my_proc = proc { passed_block_counter += 1 }
       range.my_each { my_proc.call }
       expect(passed_block_counter).to eql(range.size)
+    end
+
+    it 'Should work with ranges' do
+      expect(range.my_each_with_index { |_e, _i| }).to eql(range)
     end
 
     it 'Should yield the given block for each element and accept the index as a parameter' do
@@ -59,19 +75,62 @@ RSpec.describe Enumerable do
     it 'Should return itself if given a block' do
       expect(num_arr.my_each_with_index { |n| n }).to eql(num_arr)
     end
+
+    it 'Should accept hash\'s keys and values as parameters if block given' do
+      empty_str = ''
+      hash.my_each_with_index { |key, val, i| empty_str += key.to_s + val.to_s + i.to_s }
+      expect(empty_str).to eql('[:a, 1]0[:b, 2]1[:c, 3]2')
+    end
   end
 
   describe 'my_select' do
   end
 
   describe 'my_all?' do
-    it 'Returns false if condition is not present in array' do
+    it 'Should return false if one of the items is falsy ' do
+      expect(falsy_arr.my_all?).to be false
+    end
+
+    it 'Should return true if none of the items is falsy ' do
+      expect(num_arr.my_all?).to be true
+    end
+
+    it 'Should return false if one the items in the enum is not equal to the parameter and parameter is not a Class or a RegExp' do
+      expect(num_arr.my_all?(200)).to be false
+    end
+    
+    it 'Should return true if one the items in the enum is not equal to the parameter and parameter is not a Class or a RegExp' do
+      expect(equal_arr.my_all?(100)).to be true
+    end
+
+    it 'Returns false if one element doesn\'t pass the condition with block given in enumerable' do
       expect(num_arr.my_all? { |e| e > 0 }).to be false
     end
 
-    it 'Return true if condition is present in range' do
+    it 'Returns true if one element doesn\'t pass the condition with block given in enumerable' do
+      expect(num_arr.my_all? { |e| e < 1000 }).to be true
+    end
+
+    it 'Returns false if an item of an array is not from the same class' do
+      expect(falsy_arr.my_all?(Integer)).to be false
+    end
+    
+    it 'Return true if all items in enum are the instances of the same class' do
       expect((5..50).my_all?(Integer)).to be true
     end
+
+    it 'Returns false if an item of a hash is not from the same class' do
+      expect(hash.my_all?(Symbol)).to be false
+    end
+    
+    it 'Returns true if an item of enum does not match the RegExp' do
+      expect(num_arr.my_all?(/\d/)).to be true
+    end
+
+    it 'Returns false if an item of enum does not match the RegExp' do
+      expect(falsy_arr.my_all?(/\d/)).to be false
+    end
+
   end
 
   describe "my_any?" do
