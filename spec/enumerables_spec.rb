e201bbdb3610f994e2076053c284fea637aa4022
sarah <sarachamorro@gmail.com>
2019-10-30 18:48:14 -0300

    ADD ALL TESTS FOR MY_NONE

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -5,10 +5,16 @@ require './enumerables.rb'
 RSpec.describe Enumerable do
   let(:num_arr) { [2, 10, 1, 100, 12, -10] }
   let(:falsy_arr) { [nil, 10, 1, 100, 12, -10] }
+  let(:false_arr) { [nil, false, nil, false, false] }
+  let(:truthy_arr) { [nil, false, 1, false, nil, nil] }
   let(:equal_arr) { [100, 100, 100] }
   let(:str_arr) { ['2', '10', '1', '100', '12', '-10'] }
+  let(:sym_same_arr) { %i[1 1 1] }
+  let(:sym_dif_arr) { %i[2 b 1] }
   let(:range) { (1..5) }
   let(:hash) { { a: 1, b: 2, c: 3 } }
+  one_symbol = '1'.to_sym
+  two_symbol = '2'.to_sym
 
   describe 'my_each' do
     it 'Returns an enumerator unless a block is given' do
@@ -87,65 +93,120 @@ RSpec.describe Enumerable do
   end
 
   describe 'my_all?' do
-    it 'Should return false if one of the items is falsy ' do
+    it 'Should return false if one of the items is falsy and no block or parameter were given' do
       expect(falsy_arr.my_all?).to be false
     end
 
-    it 'Should return true if none of the items is falsy ' do
+    it 'Should return true if none of the items is falsy and no block or parameter were given' do
       expect(num_arr.my_all?).to be true
-    end
-
-    it 'Should return false if one the items in the enum is not equal to the parameter and parameter is not a Class or a RegExp' do
-      expect(num_arr.my_all?(200)).to be false
-    end
-    
-    it 'Should return true if one the items in the enum is not equal to the parameter and parameter is not a Class or a RegExp' do
-      expect(equal_arr.my_all?(100)).to be true
     end
 
     it 'Returns false if one element doesn\'t pass the condition with block given in enumerable' do
       expect(num_arr.my_all? { |e| e > 0 }).to be false
     end
 
-    it 'Returns true if one element doesn\'t pass the condition with block given in enumerable' do
+    it 'Returns true if all elements pass the condition with block given in enumerable' do
       expect(num_arr.my_all? { |e| e < 1000 }).to be true
     end
 
-    it 'Returns false if an item of an array is not from the same class' do
+    it 'Should return false if one the items in the enum is not
+    equal to the parameter and parameter is not a Class or a RegExp and no block was given' do
+      expect(num_arr.my_all?(200)).to be false
+    end
+
+    it 'Should return true if all the items in the enum are equal
+    to the parameter and parameter is not a Class or a RegExp and no block was given' do
+      expect(equal_arr.my_all?(100)).to be true
+    end
+
+    it 'Returns false if an item of an array is not an instance of the same Class and no block was given' do
       expect(falsy_arr.my_all?(Integer)).to be false
     end
-    
-    it 'Return true if all items in enum are the instances of the same class' do
+
+    it 'Return true if all items in enum are the instances of the same Class and no block was given' do
       expect((5..50).my_all?(Integer)).to be true
     end
 
-    it 'Returns false if an item of a hash is not from the same class' do
-      expect(hash.my_all?(Symbol)).to be false
+    it 'Returns true if all the items of a hash are the same Symbol as the given parameter and no block was given' do
+      expect(sym_same_arr.my_all?(one_symbol)).to be true
     end
-    
-    it 'Returns true if an item of enum does not match the RegExp' do
+
+    it 'Returns false if an item of a hash is not the same Symbol as the given parameter and no block was given' do
+      expect(sym_dif_arr.my_all?(one_symbol)).to be false
+    end
+
+    it 'Returns true if all items of enum match the RegExp parameter and no block was given' do
       expect(num_arr.my_all?(/\d/)).to be true
     end
 
-    it 'Returns false if an item of enum does not match the RegExp' do
+    it 'Returns false if an item of enum does not match the RegExp parameter and no block was given' do
       expect(falsy_arr.my_all?(/\d/)).to be false
+    end
+  end
+
+  describe 'my_any?' do
+  end
+
+  describe 'my_none?' do
+    it 'Should return false if one of the items is truthy and no block or parameter were given' do
+      expect(truthy_arr.my_none?).to be false
+    end
+
+    it 'Should return true if all of the items are falsy and no block or parameter were given' do
+      expect(false_arr.my_none?).to be true
+    end
+
+    it 'Returns false if one element passes the condition with block given in enumerable' do
+      expect(num_arr.my_none? { |e| e < 0 }).to be false
+    end
+
+    it 'Returns true if all elements don\'t pass the condition with block given in enumerable' do
+      expect(num_arr.my_none? { |e| e > 1000 }).to be true
+    end
+
+    it 'Should return false if one the items in the enum is
+    equal to the parameter and parameter is not a Class or a RegExp and no block was given' do
+      expect(num_arr.my_none?(100)).to be false
+    end
+
+    it 'Should return true if none of the items in the enum are equal
+    to the parameter and parameter is not a Class or a RegExp and no block was given' do
+      expect(equal_arr.my_none?(200)).to be true
+    end
+
+    it 'Returns false if an item of an array is an instance of the same Class given as parameter and no block was given' do
+      expect(truthy_arr.my_none?(Integer)).to be false
+    end
+
+    it 'Return true if all items in enum aren\'t instances of the same Class given as parameter and no block was given' do
+      expect((5..50).my_none?(String)).to be true
+    end
+
+    it 'Returns true if none the items of a hash are the same Symbol as the given parameter and no block was given' do
+      expect(sym_same_arr.my_none?(two_symbol)).to be true
+    end
+
+    it 'Returns false if an item of a hash is the same Symbol as the given parameter and no block was given' do
+      expect(sym_dif_arr.my_none?(two_symbol)).to be false
+    end
+
+    it 'Returns true if none of the items of enum match the RegExp parameter and no block was given' do
+      expect(num_arr.my_none?(/lol/)).to be true
+    end
+
+    it 'Returns false if an item of enum matches the RegExp parameter and no block was given' do
+      expect(falsy_arr.my_none?(/\d/)).to be false
     end
 
   end
 
-  describe "my_any?" do
+  describe 'my_count' do
   end
 
-  describe "my_none?" do
+  describe 'my_map' do
   end
 
-  describe "my_count" do
-  end
-
-  describe "my_map" do
-  end
-
-  describe "my_inject" do
+  describe 'my_inject' do
   end
 end
 
