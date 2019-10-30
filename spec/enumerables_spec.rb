5c2a861f029da9904b2fa8e1a219d0f891d419d9
sarah <sarachamorro@gmail.com>
2019-10-30 14:41:06 -0300

    FIRST COMMIT

---
+++ spec/enumerables_spec.rb
@@ -0,0 +1,50 @@
+#spec/enumerables_spec.rb
+
+require './enumerables.rb'
+
+RSpec.describe Enumerable do
+  let(:num_arr) { [2, 10, 1, 100, 12, -10] }
+  let(:str_arr) { ['2', '10', '1', '100', '12', '-10'] }
+
+  describe 'my_each' do
+    empty_str = ''
+
+    it 'Returns an enumerator unless a block is given' do
+      expect(num_arr.my_each).to be_kind_of(Enumerator)
+    end
+
+    it 'Puts each item of the enumerable object if a block is given' do
+      str_arr.my_each { |n| empty_str += n }
+      expect(empty_str).to eql('210110012-10')
+    end
+
+    it "should test something" do
+      # just an example
+      expect num_arr.my_each { |b| puts(&b) }.to yield_successive_args
+    end
+  end
+
+  describe "my_each_with_index" do
+  end
+
+  describe "my_select" do
+  end
+
+  describe "my_all?" do
+  end
+
+  describe "my_any?" do
+  end
+
+  describe "my_none?" do
+  end
+
+  describe "my_count" do
+  end
+
+  describe "my_map" do
+  end
+
+  describe "my_inject" do
+  end
+end
